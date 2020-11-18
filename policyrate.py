1242a800087c826ab88969842b2ba6128d394764
Julie <pinkjc04@gmail.com>
2020-11-18 18:25:39 -0500

    trial run

--- policyrate.py
+++ policyrate.py
@@ -6,3 +6,13 @@ import requests
 import io
 from vega_datasets import data
 
+url="https://raw.githubusercontent.com/CMU-IDS-2020/fp-squirrels-in-my-pants/main/police_policies.csv"
+s=requests.get(url).content
+c=pd.read_csv(io.StringIO(s.decode('utf-8')))
+
+trial = alt.Chart(c).mark_circle().encode(
+    alt.X('City:N', bin=True),
+    alt.Y('Total:Q', bin=True),
+    #size='count()'
+)
+st.write(trial)
