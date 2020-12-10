51621bedd750701484dce682c30e51892c4a12af
Asad Sheikh <asadalis@andrew.cmu.edu>
2020-12-09 19:01:30 -0600

    making changes !

--- js/main.js
+++ js/main.js
@@ -18,10 +18,13 @@ function fill_grid(fill_amt) {
 
     for(var i=fill_amt+1; i<=100; i++) {
       cell = document.querySelector('#i' + i);
-      if (cell.classList.contains('blank')) {
-        cell.style.color = 'black';
-      } else {
+      if (!cell.classList.contains("partial_fill")) {
+        cell.querySelector('#percent').setAttribute('x','100%');    
+      }
+      if (cell.classList.contains('selected')) {
         cell.style.color = 'red';
+      } else {
+        cell.style.color = 'black';
       }
     }
   }
@@ -127,5 +130,26 @@ grid = make_grid(fill_grid, choose_answer);
 officer_grid.appendChild(grid);
 
 function showResults() {
+  var grid = document.createElement('table');
+  grid.className = 'grid';
+
+  var blank = `
+    <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-person-fill" xmlns="http://www.w3.org/2000/svg">
+      <path fill="currentColor" fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+    </svg>`
   
+  let i = 0
+  for (var r=0; r<10; ++r) {
+    var tr = grid.appendChild(document.createElement('tr'));
+    for (var c=0; c<10; ++c){
+      var cell = tr.appendChild(document.createElement('td'));
+      i++
+      cell.setAttribute('id', 'j'+i);
+      cell.style.color = 'black';
+      cell.insertAdjacentHTML('beforeend', blank);
+    }
+  }
+  button = document.querySelector('#guess-btn');
+  button.remove();
+  document.querySelector('#soln-grid').appendChild(grid);
 }
